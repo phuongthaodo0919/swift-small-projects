37af92bb6c163891d77a22e5070868b46c51c8db
Salm Do <thaodo.usa@gmail.com>
2023-04-20 08:20:15 -0500

    Initial Commit

---
+++ SmallProjects/SmallProjectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmallProjectsApp.swift
+//  SmallProjects
+//
+//  Created by Salmdo on 4/20/23.
+//
+
+import SwiftUI
+
+@main
+struct SmallProjectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
